c9ed66e033d6f7b347018fdf57c018ba2b67bc54
Utaka19 <185202718+Utaka19@users.noreply.github.com>
2026-05-01 17:24:30 +0900

    ESLintの設定ファイルを作成

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,50 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require("eslint-config-expo/flat");
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import expo from "eslint-config-expo";
 
-module.exports = defineConfig([
-  expoConfig,
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  expo,
+
   {
-    ignores: ["dist/*"],
-  }
-]);
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: { attributes: false },
+        },
+      ],
+      "@typescript-eslint/no-var-requires": "off",
+    },
+  },
+
+  {
+    ignores: [
+      "babel.config.js",
+      // "metro.config.js"
+    ],
+  },
+];
