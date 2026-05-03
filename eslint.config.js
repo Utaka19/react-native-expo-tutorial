9df2959463a34bb6d909b38e159a7526a9eda29a
Utaka19 <185202718+Utaka19@users.noreply.github.com>
2026-05-03 15:46:03 +0900

    Imageコンポーネントを追加して画像を表示

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,8 @@ export default [
       },
     },
     rules: {
+      "@typescript-eslint/no-require-imports": "off",
+
       "@typescript-eslint/consistent-type-imports": [
         "warn",
         {
